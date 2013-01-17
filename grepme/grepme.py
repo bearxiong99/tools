49d64fcfad311f72c23827a8594e09eaca205f36
Marcus Lunden <marcus@thingsquare.com>
2013-01-17 18:01:23 +0100

    renamed findme into grepme as it uses grep, not find

---
+++ grepme/grepme.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+# 
+# Author: Marcus Lunden <marcus@bithappens.se>
+#
+#
+import sys
+import os
+
+#-------------------------------------------------------------------------------
+def bailout(errstr = "", exitcode = 1):
+  if exitcode != 0:
+    print("will grep (recursively if no files specified) for your search string.")
+    print("usage: " + sys.argv[0] + " \"search string\" FILES")
+    print("Examples: ----------")
+    print("  " + sys.argv[0] + " hej")
+    print("  " + sys.argv[0] + " hej *.c")
+    print("  " + sys.argv[0] + " \"hej\" *.c")
+    print("  " + sys.argv[0] + " \"hello world\" *.c")
+    print("  " + sys.argv[0] + " \"hello world\"")
+    print("")
+    print("*** error ***: " + errstr)
+  sys.exit(exitcode)
+#-------------------------------------------------------------------------------
+# was intended for checking the input argument but it seems it worked pertty well
+# anyway, so I just reduced this to accept anything. Kept it here in case I later
+# encounter anything to check for
+def check_ok(arg = ''):
+  return True
+# ------------------------------------------------------------------------------
+def main():
+  command = "grep -r --color=auto -n "
+  
+  if len(sys.argv) == 1:
+    bailout("", 0)
+
+  if len(sys.argv) == 2:
+    if check_ok(sys.argv[1]):
+      command += '"' + sys.argv[1] + '" *'
+    else:
+      bailout("error in argument", 1)
+
+  if len(sys.argv) >= 3:
+    # the term to search for
+    if check_ok(sys.argv[1]):
+      command += '"' + sys.argv[1] + '"' + " "
+    else:
+      bailout("error in argument", 1)
+    for arg in sys.argv[2:]:
+      command += arg + " "
+
+#  print(command)
+  # execute OS command
+#  print(os.getcwd())
+  os.system(command)
+
+# ------------------------------------------------------------------------------
+if __name__=="__main__":
+    main()
+
+# ------------------------------------------------------------------------------
+
+
